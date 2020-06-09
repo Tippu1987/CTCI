a9e56350ec912216949989fde27f04bd651b0548
Pavan Kumar Kanukollu <pavan.kanukollu@gmail.com>
2020-06-08 21:59:28 -0700

    Prob 1.1 implemented

--- Arrays & Strings/Prob1.1/Program.cs
+++ Arrays & Strings/Prob1.1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
 
 namespace Prob1._1
 {
@@ -12,7 +14,7 @@ namespace Prob1._1
             string inputStr = Console.ReadLine();
             Stopwatch stp = new Stopwatch();
             stp.Start();
-            if (isUnique(inputStr))
+            if (isUniqueNoExtraMemory(inputStr))
                 Console.WriteLine($"Input String is Char Unique: {stp.ElapsedMilliseconds}");
             else
                 Console.WriteLine($"Input String is NOT Char Unique: {stp.ElapsedMilliseconds}");
@@ -26,7 +28,7 @@ namespace Prob1._1
             else
             {
                 Dictionary<char, int> charDict = new Dictionary<char, int>();
-                for(int i = 0; i < inputStr.Length; i++)
+                for (int i = 0; i < inputStr.Length; i++)
                 {
                     if (!charDict.ContainsKey(inputStr[i]))
                         charDict.Add(inputStr[i], 1);
@@ -36,5 +38,49 @@ namespace Prob1._1
                 return true;
             }
         }
+        public static bool isUniqueAssumingAsciiString(string inputStr)
+        {
+            if (string.IsNullOrEmpty(inputStr))
+                return false;
+            else
+            {
+                bool[] resultArray = new bool[128];
+                for (int i = 0; i < resultArray.Length; i++)
+                {
+                    resultArray[i] = false;
+                }
+                for (int i = 0; i < inputStr.Length; i++)
+                {
+                    int charAt = inputStr[i];
+                    if (resultArray[charAt])
+                        return false;
+                    else
+                        resultArray[charAt] = true;
+                }
+                return true;
+            }
+
+
+
+        }
+
+        public static bool isUniqueNoExtraMemory(string inputStr)
+        {
+            if (string.IsNullOrEmpty(inputStr))
+                return false;
+            else
+            {
+                for (int i = 0; i < inputStr.Length; i++)
+                {
+                    for (int j = i+1; j < inputStr.Length; j++)
+                    {
+                        if (inputStr[j] == inputStr[i])
+                            return false;
+                    }
+                }
+                return true;
+            }
+        }
+        //Shud Clear the Bitwise Operation Solution..:(
     }
 }
