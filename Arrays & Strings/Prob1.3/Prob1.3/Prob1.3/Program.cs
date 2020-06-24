e72fb4c3630fd897921cdee49af33197008cece8
Pavan Kumar Kanukollu <pavan.kanukollu@gmail.com>
2020-06-23 21:27:24 -0700

    Prob 1.3

---
+++ Arrays & Strings/Prob1.3/Prob1.3/Prob1.3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace Prob1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Input String");
+            string input = Console.ReadLine();
+            Console.WriteLine(URLify(input));
+        }
+
+        private static string URLify(string input)
+        {
+            if (!string.IsNullOrEmpty(input))
+            {
+                StringBuilder sb = new StringBuilder();
+                int startIndex = 0, endIndex = input.Length - 1;
+                while (input[startIndex] == ' ' && startIndex < endIndex)
+                {
+                    startIndex++;
+                }
+                while (input[endIndex] == ' ' && startIndex <= endIndex)
+                {
+                    endIndex--;
+                }
+                for (int i = startIndex; i < endIndex; i++)
+                {
+                    if (input[i] == ' ')
+                    {
+                        sb.Append("%20");
+                    }
+                    else
+                        sb.Append(input[i]);
+                }
+
+                return sb.ToString();
+            }
+            else
+                return "Empty String or NULL";
+        }
+    }
+}
