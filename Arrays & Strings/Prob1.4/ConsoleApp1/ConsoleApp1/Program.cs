7e88cbd0f02333549d08eab0bdacc3c1b979c69c
Pavan Kumar Kanukollu <pavan.kanukollu@gmail.com>
2020-06-24 22:22:05 -0700

    1.4 Implementation of HashTable

---
+++ Arrays & Strings/Prob1.4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the String");
+            string input = Console.ReadLine();
+            Console.WriteLine(IsPalindromePermutation(input));
+        }
+
+        private static bool IsPalindromePermutation(string input)
+        {
+            bool bret = true;
+            if (string.IsNullOrEmpty(input))
+            {
+                bret = true;
+            }
+            else
+            {
+                Dictionary<char, int> charCount = new Dictionary<char, int>();
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if(input[i] != ' ')
+                    {
+                        if (charCount.ContainsKey(input[i]))
+                            charCount[input[i]] = charCount[input[i]] + 1;
+                        else
+                            charCount.Add(input[i], 1);
+                    }
+                    
+                }
+
+                bool oddcount = false;
+                foreach (var kvp in charCount)
+                {
+                    if (kvp.Value % 2 == 0)
+                        continue;
+                    else
+                    {
+                        if (!oddcount)
+                        {
+                            oddcount = true;
+                            continue;
+                        }
+                        else
+                        {
+                            bret = false;
+                            break;
+                        }
+                    }
+                }
+            }
+            return bret;
+        }
+    }
+}
